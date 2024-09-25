fd1e32cc2b6549db9528494eb38ac554f78e08d1
gaurav sandhu <gaurav.sandhu@codebrewinnovations.com>
2024-09-25 19:26:05 +0530

    update

---
+++ backend/config/database.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require('mongodb');
+
+const url = 'mongodb+srv://developerashar:4LjR#XSqS$Yd$4mX%J2W@cluster0.l8dpaxe.mongodb.net/';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'super-dooper-spoon';
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('documents');
+
+  // the following code examples can be pasted here...
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
